3cbb2fa238a64d76f3fdfc3d17a7a0fa7469e89f
udinz <tamvanzudin@gmail.com>
2025-07-08 08:15:29 +0700

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'gustafhosting@f5.si';
+  $receiving_email_address = 'adityagustaf35@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
